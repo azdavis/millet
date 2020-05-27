4c91af7f7dfdd12e0b43d27facf67758569018cc
Ariel Davis <ariel.z.davis@icloud.com>
2020-05-27 16:22:08 -0700

    Fill in main a bit

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -2,7 +2,25 @@ mod args;
 
 fn run() -> bool {
   let args = args::get();
-  println!("args = {:?}", args);
+  let stdout = std::io::stdout();
+  let stdout = stdout.lock();
+  let mut m = millet_core::source::SourceMap::new();
+  let mut w = millet_core::source::Reporter::new(stdout);
+  for name in args.files {
+    match std::fs::read_to_string(&name) {
+      Ok(s) => m.insert(name, s),
+      Err(e) => {
+        w.report_io(&name, e).unwrap();
+        return false;
+      }
+    }
+  }
+  for _ in m
+    .iter()
+    .map(|(id, file)| millet_core::lex::get(id, file.as_bytes()))
+  {
+    //
+  }
   true
 }
 
