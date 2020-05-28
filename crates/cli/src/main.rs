cec0376c40f65297b648c19919822d9033602652
Ariel Davis <ariel.z.davis@icloud.com>
2020-05-28 14:43:53 -0700

    Use

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,11 +1,13 @@
 mod args;
 
+use millet_core::lex;
+use millet_core::source::{Reporter, SourceMap};
+
 fn run() -> bool {
   let args = args::get();
   let stdout = std::io::stdout();
-  let stdout = stdout.lock();
-  let mut m = millet_core::source::SourceMap::new();
-  let mut w = millet_core::source::Reporter::new(stdout);
+  let mut m = SourceMap::new();
+  let mut w = Reporter::new(stdout.lock());
   for name in args.files {
     match std::fs::read_to_string(&name) {
       Ok(s) => m.insert(name, s),
@@ -15,10 +17,7 @@ fn run() -> bool {
       }
     }
   }
-  for _ in m
-    .iter()
-    .map(|(id, file)| millet_core::lex::get(id, file.as_bytes()))
-  {
+  for _ in m.iter().map(|(id, file)| lex::get(id, file.as_bytes())) {
     //
   }
   true
