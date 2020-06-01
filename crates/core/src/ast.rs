fe887be61e285db7aa499ac50329aa0f0cef3af4
Ariel Davis <ariel.z.davis@icloud.com>
2020-05-31 23:32:29 -0700

    Add Located to many ast things

--- crates/core/src/ast.rs
+++ crates/core/src/ast.rs
@@ -1,3 +1,4 @@
+use crate::source::Located;
 use crate::token::TyVar;
 
 pub enum Exp<I> {
@@ -14,33 +15,37 @@ pub enum Exp<I> {
   Record(Vec<Row<I>>),
   Select(Label),
   /// requires vec.len() != 1
-  Tuple(Vec<Exp<I>>),
-  List(Vec<Exp<I>>),
+  Tuple(Vec<Located<Exp<I>>>),
+  List(Vec<Located<Exp<I>>>),
   /// requires vec.len() >= 2
-  Sequence(Vec<Exp<I>>),
+  Sequence(Vec<Located<Exp<I>>>),
   /// requires vec.len != 0
-  Let(Box<Dec<I>>, Vec<Exp<I>>),
-  App(Box<Exp<I>>, Box<Exp<I>>),
-  InfixApp(Box<Exp<I>>, I, Box<Exp<I>>),
-  Typed(Box<Exp<I>>, Ty<I>),
-  Andalso(Box<Exp<I>>, Box<Exp<I>>),
-  Orelse(Box<Exp<I>>, Box<Exp<I>>),
-  Handle(Box<Exp<I>>, Match<I>),
-  Raise(Box<Exp<I>>),
-  If(Box<Exp<I>>, Box<Exp<I>>, Box<Exp<I>>),
-  While(Box<Exp<I>>, Box<Exp<I>>),
-  Case(Box<Exp<I>>, Match<I>),
+  Let(Box<Located<Dec<I>>>, Vec<Located<Exp<I>>>),
+  App(Box<Located<Exp<I>>>, Box<Located<Exp<I>>>),
+  InfixApp(Box<Located<Exp<I>>>, Located<I>, Box<Located<Exp<I>>>),
+  Typed(Box<Located<Exp<I>>>, Located<Ty<I>>),
+  Andalso(Box<Located<Exp<I>>>, Box<Located<Exp<I>>>),
+  Orelse(Box<Located<Exp<I>>>, Box<Located<Exp<I>>>),
+  Handle(Box<Located<Exp<I>>>, Match<I>),
+  Raise(Box<Located<Exp<I>>>),
+  If(
+    Box<Located<Exp<I>>>,
+    Box<Located<Exp<I>>>,
+    Box<Located<Exp<I>>>,
+  ),
+  While(Box<Located<Exp<I>>>, Box<Located<Exp<I>>>),
+  Case(Box<Located<Exp<I>>>, Match<I>),
   Fn(Match<I>),
 }
 
 pub struct Long<I> {
   /// requires !idents.is_empty()
-  pub idents: Vec<I>,
+  pub idents: Vec<Located<I>>,
 }
 
 pub struct Row<I> {
-  pub lab: Label,
-  pub exp: Exp<I>,
+  pub lab: Located<Label>,
+  pub exp: Located<Exp<I>>,
 }
 
 pub enum Label {
@@ -53,41 +58,41 @@ pub struct Match<I> {
 }
 
 pub struct Arm<I> {
-  pub pat: Pat<I>,
-  pub exp: Exp<I>,
+  pub pat: Located<Pat<I>>,
+  pub exp: Located<Exp<I>>,
 }
 
 pub enum Dec<I> {
   /// requires !val_binds.is_empty()
-  Val(Vec<TyVar<I>>, Vec<ValBind<I>>),
+  Val(Vec<Located<TyVar<I>>>, Vec<ValBind<I>>),
   /// requires !fval_binds.is_empty()
-  Fun(Vec<TyVar<I>>, Vec<FValBind<I>>),
+  Fun(Vec<Located<TyVar<I>>>, Vec<FValBind<I>>),
   /// requires !ty_binds.is_empty()
   Type(Vec<TyBind<I>>),
   /// requires !dat_binds.is_empty()
   Datatype(Vec<DatBind<I>>, Vec<TyBind<I>>),
-  DatatypeCopy(I, Long<I>),
+  DatatypeCopy(Located<I>, Long<I>),
   /// requires !dat_binds.is_empty()
-  Abstype(Vec<DatBind<I>>, Vec<TyBind<I>>, Box<Dec<I>>),
+  Abstype(Vec<DatBind<I>>, Vec<TyBind<I>>, Box<Located<Dec<I>>>),
   /// requires !ex_binds.is_empty()
   Exception(Vec<ExBind<I>>),
-  Local(Box<Dec<I>>, Box<Dec<I>>),
+  Local(Box<Located<Dec<I>>>, Box<Located<Dec<I>>>),
   /// requires !opens.is_empty()
   Open(Vec<Long<I>>),
   /// requires decs.len() >= 2
-  Seq(Vec<Dec<I>>),
+  Seq(Vec<Located<Dec<I>>>),
   /// requires !vids.is_empty()
-  Infix(u32, Vec<I>),
+  Infix(Located<u32>, Vec<Located<I>>),
   /// requires !vids.is_empty()
-  Infixr(u32, Vec<I>),
+  Infixr(Located<u32>, Vec<Located<I>>),
   /// requires !vids.is_empty()
-  Nonfix(Vec<I>),
+  Nonfix(Vec<Located<I>>),
 }
 
 pub struct ValBind<I> {
   pub rec: bool,
-  pub pat: Pat<I>,
-  pub exp: Exp<I>,
+  pub pat: Located<Pat<I>>,
+  pub exp: Located<Exp<I>>,
 }
 
 pub struct FValBind<I> {
@@ -96,38 +101,38 @@ pub struct FValBind<I> {
 }
 
 pub struct FValBindCase<I> {
-  pub vid: I,
+  pub vid: Located<I>,
   /// requires !pats.is_empty()
-  pub pats: Vec<Pat<I>>,
-  pub ret_ty: Option<Ty<I>>,
-  pub body: Exp<I>,
+  pub pats: Vec<Located<Pat<I>>>,
+  pub ret_ty: Option<Located<Ty<I>>>,
+  pub body: Located<Exp<I>>,
 }
 
 pub struct TyBind<I> {
-  pub ty_vars: Vec<TyVar<I>>,
-  pub ty_con: I,
-  pub ty: Ty<I>,
+  pub ty_vars: Vec<Located<TyVar<I>>>,
+  pub ty_con: Located<I>,
+  pub ty: Located<Ty<I>>,
 }
 
 pub struct DatBind<I> {
-  pub ty_vars: Vec<TyVar<I>>,
-  pub ty_con: I,
+  pub ty_vars: Vec<Located<TyVar<I>>>,
+  pub ty_con: Located<I>,
   /// requires !cons.is_empty()
   pub cons: Vec<ConBind<I>>,
 }
 
 pub struct ConBind<I> {
-  pub vid: I,
-  pub ty: Option<Ty<I>>,
+  pub vid: Located<I>,
+  pub ty: Option<Located<Ty<I>>>,
 }
 
 pub struct ExBind<I> {
-  pub vid: I,
+  pub vid: Located<I>,
   pub inner: Option<ExBindInner<I>>,
 }
 
 pub enum ExBindInner<I> {
-  Ty(Ty<I>),
+  Ty(Located<Ty<I>>),
   Long(Long<I>),
 }
 
@@ -143,66 +148,71 @@ pub enum Pat<I> {
   Char(u8),
   // end special constants
   LongVid(Long<I>),
-  Record(Vec<PatRow<I>>),
+  Record(Vec<Located<PatRow<I>>>),
   /// requires pats.len() != 1
-  Tuple(Vec<Pat<I>>),
-  List(Vec<Pat<I>>),
-  Ctor(Long<I>, Box<Pat<I>>),
-  InfixCtor(Box<Pat<I>>, I, Box<Pat<I>>),
-  Typed(Box<Pat<I>>, Ty<I>),
-  As(I, Option<Ty<I>>, Box<Pat<I>>),
+  Tuple(Vec<Located<Pat<I>>>),
+  List(Vec<Located<Pat<I>>>),
+  Ctor(Long<I>, Box<Located<Pat<I>>>),
+  InfixCtor(Box<Located<Pat<I>>>, Located<I>, Box<Located<Pat<I>>>),
+  Typed(Box<Located<Pat<I>>>, Located<Ty<I>>),
+  As(Located<I>, Option<Located<Ty<I>>>, Box<Located<Pat<I>>>),
 }
 
 pub enum PatRow<I> {
   DotDotDot,
-  LabelAndPat(Label, Pat<I>),
-  LabelAsVid(I, Option<Ty<I>>, Option<Pat<I>>),
+  LabelAndPat(Located<Label>, Located<Pat<I>>),
+  LabelAsVid(Located<I>, Option<Located<Ty<I>>>, Option<Located<Pat<I>>>),
 }
 
 pub enum Ty<I> {
   TyVar(TyVar<I>),
   Record(Vec<TyRow<I>>),
-  TyCon(Vec<Ty<I>>, Long<I>),
+  TyCon(Vec<Located<Ty<I>>>, Long<I>),
   /// requires tys.len() >= 2
-  Tuple(Vec<Ty<I>>),
-  Arrow(Box<Ty<I>>, Box<Ty<I>>),
+  Tuple(Vec<Located<Ty<I>>>),
+  Arrow(Box<Located<Ty<I>>>, Box<Located<Ty<I>>>),
 }
 
 pub struct TyRow<I> {
-  pub lab: Label,
-  pub ty: Ty<I>,
+  pub lab: Located<Label>,
+  pub ty: Located<Ty<I>>,
 }
 
 pub enum StrExp<I> {
-  Struct(StrDec<I>),
+  Struct(Located<StrDec<I>>),
   LongStrId(Long<I>),
-  Transparent(Box<StrExp<I>>, SigExp<I>),
-  Opaque(Box<StrExp<I>>, SigExp<I>),
-  FunctorApp(Long<I>, Box<StrExp<I>>),
-  Let(StrDec<I>, Box<StrExp<I>>),
+  Transparent(Box<Located<StrExp<I>>>, Located<SigExp<I>>),
+  Opaque(Box<Located<StrExp<I>>>, Located<SigExp<I>>),
+  FunctorApp(Long<I>, Box<Located<StrExp<I>>>),
+  Let(Located<StrDec<I>>, Box<Located<StrExp<I>>>),
 }
 
 pub enum StrDec<I> {
-  Dec(Dec<I>),
+  Dec(Located<Dec<I>>),
   /// requires !str_binds.is_empty()
   Structure(Vec<StrBind<I>>),
-  Local(Box<StrDec<I>>, Box<StrDec<I>>),
+  Local(Box<Located<StrDec<I>>>, Box<Located<StrDec<I>>>),
 }
 
 pub struct StrBind<I> {
-  pub strid: I,
-  pub exp: StrExp<I>,
+  pub strid: Located<I>,
+  pub exp: Located<StrExp<I>>,
 }
 
 pub enum SigExp<I> {
-  Sig(Spec<I>),
+  Sig(Located<Spec<I>>),
   SigId(Long<I>),
-  Where(Box<SigExp<I>>, Vec<TyVar<I>>, Long<I>, Ty<I>),
+  Where(
+    Box<Located<SigExp<I>>>,
+    Vec<Located<TyVar<I>>>,
+    Long<I>,
+    Located<Ty<I>>,
+  ),
 }
 
 pub struct SigBind<I> {
-  pub sigid: I,
-  pub exp: SigExp<I>,
+  pub sigid: Located<I>,
+  pub exp: Located<SigExp<I>>,
 }
 
 pub enum Spec<I> {
@@ -214,57 +224,57 @@ pub enum Spec<I> {
   Eqtype(Vec<TyDesc<I>>),
   /// requires !dat_descs.is_empty()
   Datatype(Vec<DatDesc<I>>),
-  DatatypeCopy(I, Long<I>),
+  DatatypeCopy(Located<I>, Long<I>),
   /// requires !ex_descs.is_empty()
   Exception(Vec<ExDesc<I>>),
   /// requires !str_descs.is_empty()
   Structure(Vec<StrDesc<I>>),
-  Include(Box<SigExp<I>>),
+  Include(Box<Located<SigExp<I>>>),
   /// requires specs.len() != 1
-  Seq(Vec<Spec<I>>),
+  Seq(Vec<Located<Spec<I>>>),
 }
 
 pub struct ValDesc<I> {
-  pub vid: I,
-  pub ty: Ty<I>,
+  pub vid: Located<I>,
+  pub ty: Located<Ty<I>>,
 }
 
 pub struct TyDesc<I> {
-  pub ty_vars: Vec<TyVar<I>>,
-  pub ty_con: I,
+  pub ty_vars: Vec<Located<TyVar<I>>>,
+  pub ty_con: Located<I>,
 }
 
 pub struct DatDesc<I> {
-  pub ty_vars: Vec<TyVar<I>>,
-  pub ty_con: I,
+  pub ty_vars: Vec<Located<TyVar<I>>>,
+  pub ty_con: Located<I>,
   /// requires !con_descs.is_empty()
   pub con_descs: Vec<ConDesc<I>>,
 }
 
 pub struct ConDesc<I> {
-  pub vid: I,
-  pub ty: Option<Ty<I>>,
+  pub vid: Located<I>,
+  pub ty: Option<Located<Ty<I>>>,
 }
 
 pub struct ExDesc<I> {
-  pub vid: I,
-  pub ty: Option<Ty<I>>,
+  pub vid: Located<I>,
+  pub ty: Option<Located<Ty<I>>>,
 }
 
 pub struct StrDesc<I> {
-  pub str_id: I,
-  pub exp: SigExp<I>,
+  pub str_id: Located<I>,
+  pub exp: Located<SigExp<I>>,
 }
 
 pub struct FunBind<I> {
   pub fun_id: Long<I>,
   pub str_id: Long<I>,
-  pub sig_exp: SigExp<I>,
-  pub str_exp: StrExp<I>,
+  pub sig_exp: Located<SigExp<I>>,
+  pub str_exp: Located<StrExp<I>>,
 }
 
 pub enum TopDec<I> {
-  StrDec(StrDec<I>),
+  StrDec(Located<StrDec<I>>),
   /// requires !sig_binds.is_empty()
   SigDec(Vec<SigBind<I>>),
   /// requires !fun_binds.is_empty()
