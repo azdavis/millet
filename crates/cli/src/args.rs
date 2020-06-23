944d3384e388aee2e5f1dc4c1cd511d3bfd852e0
Ariel Davis <ariel.z.davis@icloud.com>
2020-06-22 22:08:30 -0700

    Doc

--- crates/cli/src/args.rs
+++ crates/cli/src/args.rs
@@ -2,6 +2,8 @@
 
 use gumdrop::Options;
 
+/// Returns the command-line arguments parsed into an `Args` struct, or exit the process if the
+/// command-line arguments were invalid or if the help flag was passed.
 pub fn get() -> Args {
   Args::parse_args_default_or_exit()
 }
@@ -10,6 +12,7 @@ pub fn get() -> Args {
 pub struct Args {
   #[options(free, help = "Source files")]
   pub files: Vec<String>,
+  /// Always `false` since `get` will exit the process if the help flag is detected.
   #[options(help = "Show this help")]
   pub help: bool,
   #[options(help = "Show the version")]
