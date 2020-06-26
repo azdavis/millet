dfae6f1037393a0dcff8e32eba00e52fbbf3ea15
Ariel Davis <ariel.z.davis@icloud.com>
2020-06-25 22:29:05 -0700

    Continue hacking

--- extensions/vscode/src/main.ts
+++ extensions/vscode/src/main.ts
@@ -1,1 +1,14 @@
-console.log("hello from millet vscode");
+import * as vscode from "vscode";
+
+console.log("got here 123");
+
+export function activate(context: vscode.ExtensionContext) {
+  console.log("got here 234");
+  const disposable = vscode.commands.registerCommand(
+    "extension.helloWorld",
+    () => {
+      vscode.window.showInformationMessage("Hello World!");
+    },
+  );
+  context.subscriptions.push(disposable);
+}
