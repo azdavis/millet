966311704e880a1af99299aad3335b3496c91260
Ariel Davis <ariel.z.davis@icloud.com>
2020-06-25 23:11:18 -0700

    Rm hello world command

--- extensions/vscode/src/main.ts
+++ extensions/vscode/src/main.ts
@@ -1,14 +1,5 @@
 import * as vscode from "vscode";
 
-console.log("got here 123");
-
-export function activate(context: vscode.ExtensionContext) {
-  console.log("got here 234");
-  const disposable = vscode.commands.registerCommand(
-    "extension.helloWorld",
-    () => {
-      vscode.window.showInformationMessage("Hello World!");
-    },
-  );
-  context.subscriptions.push(disposable);
+export function activate(cx: vscode.ExtensionContext) {
+  console.log("activated millet extension");
 }
