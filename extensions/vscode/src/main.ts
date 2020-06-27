ec763bf4d8dd03276c7c2133aad05f453743697b
Ariel Davis <ariel.z.davis@icloud.com>
2020-06-26 20:43:00 -0700

    Begin to fill in main.ts

--- extensions/vscode/src/main.ts
+++ extensions/vscode/src/main.ts
@@ -1,5 +1,12 @@
-import * as vscode from "vscode";
+import { ExtensionContext } from "vscode";
+import { LanguageClient } from "vscode-languageclient";
+import * as path from "path";
 
-export function activate(cx: vscode.ExtensionContext) {
-  console.log("activated millet extension");
+export function activate(cx: ExtensionContext) {
+  console.log("millet: activate");
+  const serverOpts = {
+    command: cx.asAbsolutePath(path.join("out", "millet-ls")),
+  };
+  const client = new LanguageClient("millet", serverOpts, {}, true);
+  cx.subscriptions.push(client.start());
 }
