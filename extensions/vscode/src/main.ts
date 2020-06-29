c620d2938a7f6f8531091f74ac4e55ac6b910e68
Ariel Davis <ariel.z.davis@icloud.com>
2020-06-28 18:12:08 -0700

    deactivate

--- extensions/vscode/src/main.ts
+++ extensions/vscode/src/main.ts
@@ -2,14 +2,24 @@ import { ExtensionContext } from "vscode";
 import { LanguageClient } from "vscode-languageclient";
 import * as path from "path";
 
+let client: LanguageClient | null;
+
 export function activate(cx: ExtensionContext) {
-  console.log("millet: activate");
+  if (client !== null) {
+    console.log("millet-ls: error: cannot re-activate");
+    return;
+  }
+  console.log("millet-ls: activate");
   const serverOpts = {
     command: cx.asAbsolutePath(path.join("out", "millet-ls")),
   };
   const clientOpts = {
     documentSelector: [{ scheme: "file", language: "sml" }],
   };
-  const client = new LanguageClient("millet-ls", serverOpts, clientOpts, true);
+  client = new LanguageClient("millet-ls", serverOpts, clientOpts, true);
   cx.subscriptions.push(client.start());
+}
+
+export function deactivate(): Promise<void> {
+  return client === null ? Promise.resolve() : client.stop();
 }
