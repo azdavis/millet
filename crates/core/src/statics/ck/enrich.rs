51db8e4212d455c2f6af3c3ec222be9100f7a375
Ariel Davis <ariel.z.davis@icloud.com>
2020-07-16 17:01:35 -0700

    Use Error::Todo instead of todo!

--- crates/core/src/statics/ck/enrich.rs
+++ crates/core/src/statics/ck/enrich.rs
@@ -14,19 +14,19 @@ pub fn ck(loc: Loc, sym_tys: &SymTys, got: &Env, want: &Env) -> Result<()> {
   // BTreeMaps, not HashMaps. See types.rs.
   for (name, want) in want.str_env.iter() {
     match got.str_env.get(name) {
-      None => todo!("missing a struct"),
+      None => return Err(loc.wrap(Error::Todo("missing a struct"))),
       Some(got) => ck(loc, sym_tys, got, want)?,
     }
   }
   for (name, want) in want.ty_env.inner.iter() {
     match got.ty_env.inner.get(name) {
-      None => todo!("missing a type"),
-      Some(got) => ck_ty_info(sym_tys, got, want)?,
+      None => return Err(loc.wrap(Error::Todo("missing a type"))),
+      Some(got) => ck_ty_info(loc, sym_tys, got, want)?,
     }
   }
   for (name, want) in want.val_env.iter() {
     match got.val_env.get(name) {
-      None => todo!("missing a value"),
+      None => return Err(loc.wrap(Error::Todo("missing a value"))),
       Some(got) => ck_val_info(loc, sym_tys, got, want)?,
     }
   }
@@ -35,14 +35,14 @@ pub fn ck(loc: Loc, sym_tys: &SymTys, got: &Env, want: &Env) -> Result<()> {
 
 fn ck_val_info(loc: Loc, sym_tys: &SymTys, got: &ValInfo, want: &ValInfo) -> Result<()> {
   if got.id_status != want.id_status && !want.id_status.is_val() {
-    todo!("incompatible id statuses")
+    return Err(loc.wrap(Error::Todo("incompatible id statuses")));
   }
   ck_generalizes(loc, sym_tys, want.ty_scheme.clone(), got.ty_scheme.clone())?;
   Ok(())
 }
 
-fn ck_ty_info(sym_tys: &SymTys, got: &TyInfo, want: &TyInfo) -> Result<()> {
-  ck_ty_fcn_eq(got.ty_fcn(sym_tys), want.ty_fcn(sym_tys))?;
+fn ck_ty_info(loc: Loc, sym_tys: &SymTys, got: &TyInfo, want: &TyInfo) -> Result<()> {
+  ck_ty_fcn_eq(loc, sym_tys, got.ty_fcn(sym_tys), want.ty_fcn(sym_tys))?;
   let want = match want {
     TyInfo::Alias(_) => return Ok(()),
     TyInfo::Sym(sym) => &sym_tys.get(sym).unwrap().val_env,
@@ -51,27 +51,27 @@ fn ck_ty_info(sym_tys: &SymTys, got: &TyInfo, want: &TyInfo) -> Result<()> {
     return Ok(());
   }
   let got = match got {
-    TyInfo::Alias(_) => todo!("got empty want non-empty"),
+    TyInfo::Alias(_) => return Err(loc.wrap(Error::Todo("got empty want non-empty"))),
     TyInfo::Sym(sym) => &sym_tys.get(sym).unwrap().val_env,
   };
-  ck_val_env_eq(got, want)
+  ck_val_env_eq(loc, sym_tys, got, want)
 }
 
-fn ck_val_env_eq(got: &ValEnv, want: &ValEnv) -> Result<()> {
+fn ck_val_env_eq(loc: Loc, sym_tys: &SymTys, got: &ValEnv, want: &ValEnv) -> Result<()> {
   todo!()
 }
 
-fn ck_ty_fcn_eq(got: &TyFcn, want: &TyFcn) -> Result<()> {
+fn ck_ty_fcn_eq(loc: Loc, sym_tys: &SymTys, got: &TyFcn, want: &TyFcn) -> Result<()> {
   todo!()
 }
 
 /// Returns Ok(s) iff want generalizes got as per the Definition, and s is this witness to this
 /// fact. TODO is this right?
-pub fn ck_generalizes(loc: Loc, sym_tys: &SymTys, want: TyScheme, got: TyScheme) -> Result<Subst> {
+fn ck_generalizes(loc: Loc, sym_tys: &SymTys, want: TyScheme, got: TyScheme) -> Result<Subst> {
   let want_free_tvs = want.free_ty_vars();
   for tv in got.ty_vars.iter() {
     if want_free_tvs.contains(tv) {
-      todo!("no")
+      return Err(loc.wrap(Error::Todo("bad free ty var")));
     }
   }
   let mut ret = Subst::default();
