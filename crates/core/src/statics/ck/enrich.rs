c31c7620063e56d0910064a4614b2520ebff7b91
Ariel Davis <ariel.z.davis@icloud.com>
2020-07-16 17:02:06 -0700

    Try ck_val_env_eq and ck_ty_fcn_eq

--- crates/core/src/statics/ck/enrich.rs
+++ crates/core/src/statics/ck/enrich.rs
@@ -4,7 +4,10 @@
 #![allow(unused)]
 
 use crate::loc::Loc;
-use crate::statics::types::{Env, Result, Subst, SymTys, TyFcn, TyInfo, TyScheme, ValEnv, ValInfo};
+use crate::statics::types::{
+  Env, Error, Result, Subst, SymTys, TyFcn, TyInfo, TyScheme, ValEnv, ValInfo,
+};
+use crate::util::eq_iter;
 
 /// Returns Ok(()) iff got enriches want as per the Definition.
 pub fn ck(loc: Loc, sym_tys: &SymTys, got: &Env, want: &Env) -> Result<()> {
@@ -58,11 +61,25 @@ fn ck_ty_info(loc: Loc, sym_tys: &SymTys, got: &TyInfo, want: &TyInfo) -> Result
 }
 
 fn ck_val_env_eq(loc: Loc, sym_tys: &SymTys, got: &ValEnv, want: &ValEnv) -> Result<()> {
-  todo!()
+  if !eq_iter(want.keys(), got.keys()) {
+    return Err(loc.wrap(Error::Todo("unequal keys")));
+  }
+  for (name, want_vi) in want {
+    let got_vi = got.get(name).unwrap();
+    if want_vi.id_status != got_vi.id_status {
+      return Err(loc.wrap(Error::Todo("unequal id statuses")));
+    }
+    ck_ty_fcn_eq(loc, sym_tys, &got_vi.ty_scheme, &want_vi.ty_scheme)?;
+  }
+  Ok(())
 }
 
+/// Returns `Ok(())` if want = got. From the Definition: "It can be shown that lhs = rhs iff lhs >>>
+/// rhs and rhs >>> lhs."
 fn ck_ty_fcn_eq(loc: Loc, sym_tys: &SymTys, got: &TyFcn, want: &TyFcn) -> Result<()> {
-  todo!()
+  ck_generalizes(loc, sym_tys, want.clone(), got.clone())?;
+  ck_generalizes(loc, sym_tys, got.clone(), want.clone())?;
+  Ok(())
 }
 
 /// Returns Ok(s) iff want generalizes got as per the Definition, and s is this witness to this
