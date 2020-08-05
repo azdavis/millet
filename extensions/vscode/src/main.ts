827d9375343fe488fda4ba9cf25c513a354e11fe
Ariel Davis <ariel.z.davis@icloud.com>
2020-08-04 20:13:33 -0700

    Rm log, make async

--- extensions/vscode/src/main.ts
+++ extensions/vscode/src/main.ts
@@ -4,12 +4,10 @@ import * as path from "path";
 
 let client: LanguageClient | null = null;
 
-export function activate(cx: ExtensionContext) {
+export async function activate(cx: ExtensionContext) {
   if (client !== null) {
-    console.log("millet-ls: error: cannot re-activate");
     return;
   }
-  console.log("millet-ls: activate");
   const serverOpts = {
     command: cx.asAbsolutePath(path.join("out", "millet-ls")),
   };
@@ -20,11 +18,9 @@ export function activate(cx: ExtensionContext) {
   cx.subscriptions.push(client.start());
 }
 
-export function deactivate(): Promise<void> | void {
+export async function deactivate() {
   if (client === null) {
-    console.log("millet-ls: error: cannot re-deactivate");
     return;
   }
-  console.log("millet-ls: deactivate");
   return client.stop();
 }
