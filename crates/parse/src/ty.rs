e6f3ea94926ed71d5cfd6aa7288cafbd430ea471
Ariel Davis <ariel.z.davis@icloud.com>
2021-03-16 21:46:50 -0700

    Impl ty_var_seq

--- crates/parse/src/ty.rs
+++ crates/parse/src/ty.rs
@@ -1,4 +1,5 @@
 use crate::parser::{Exited, Parser};
+use crate::util::many_sep;
 use syntax::SyntaxKind as SK;
 
 pub(crate) fn ty(p: &mut Parser<'_>) -> Exited {
@@ -6,7 +7,19 @@ pub(crate) fn ty(p: &mut Parser<'_>) -> Exited {
 }
 
 pub(crate) fn ty_var_seq(p: &mut Parser<'_>) -> Exited {
-  todo!()
+  let ent = p.enter();
+  if p.at(SK::TyVar) {
+    let ent = p.enter();
+    p.bump();
+    p.exit(ent, SK::TyVarArg);
+  } else if p.at(SK::LRound) && p.peek_n(1).map_or(false, |tok| tok.kind == SK::TyVar) {
+    p.bump();
+    many_sep(p, SK::Comma, SK::TyVarArg, |p| {
+      p.eat(SK::TyVar);
+    });
+    p.eat(SK::RRound);
+  }
+  p.exit(ent, SK::TyVarSeq)
 }
 
 #[must_use]
