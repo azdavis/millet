63f11bc4e9fed44f06025a70e5fb7c24a1162f70
Ariel Davis <ariel.z.davis@icloud.com>
2021-03-28 23:27:43 -0700

    Fill in Cx

--- crates/lower/src/util.rs
+++ crates/lower/src/util.rs
@@ -1,3 +1,5 @@
+use hir::{Arenas, Name};
+
 /// Pointers between the AST and the HIR.
 #[derive(Debug)]
 pub struct Ptrs {}
@@ -7,4 +9,15 @@ pub struct Ptrs {}
 pub struct Lowered {}
 
 #[derive(Debug, Default)]
-pub(crate) struct Cx {}
+pub(crate) struct Cx {
+  pub(crate) arenas: Arenas,
+  fresh_idx: u32,
+}
+
+impl Cx {
+  pub(crate) fn fresh(&mut self) -> Name {
+    let ret = Name::new(self.fresh_idx.to_string());
+    self.fresh_idx += 1;
+    ret
+  }
+}
