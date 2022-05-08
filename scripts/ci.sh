79d7a7390cf0c98955053f81d55dbe0b3fbfbaca
Ariel Davis <ariel.z.davis@icloud.com>
2022-05-07 22:10:05 -0700

    Make a CI script

---
+++ scripts/ci.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -eux
+
+cd "$(git rev-parse --show-toplevel)"
+cargo test -- --no-run
+cargo fmt -- --check
+./scripts/ck-sml-defn.sh
+cargo clippy
+cargo test
+./scripts/run-test.sh tests/*
