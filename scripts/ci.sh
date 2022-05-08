23dab41a232f48b67c91d286044b6a30726269be
Ariel Davis <ariel.z.davis@icloud.com>
2022-05-07 22:15:32 -0700

    Use info instead of set -x

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
 cd "$(git rev-parse --show-toplevel)"
+
+info() {
+  echo "==> $1"
+}
+
+info 'build'
 cargo test --no-run
+
+info 'check formatting'
 cargo fmt -- --check
+
+info 'check sml defn refs'
 ./scripts/ck-sml-defn.sh
+
+info 'run clippy'
 cargo clippy
+
+info 'run cargo tests'
 cargo test
+
+info 'run tests in tests/'
 ./scripts/run-test.sh tests/*
