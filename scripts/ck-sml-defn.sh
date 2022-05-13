97627f8b6e0f57ee01c32a375454b3e0e5a07d62
Ariel Davis <ariel.z.davis@icloud.com>
2022-05-12 18:05:12 -0700

    Shorten ck-sml-defn

--- scripts/ck-sml-defn.sh
+++ scripts/ck-sml-defn.sh
@@ -7,43 +7,15 @@ if [ "$#" -ne 0 ]; then
   exit 1
 fi
 
-mk_temp_dir() {
-  while true; do
-    name="/tmp/$(od -A n -N 8 -t u /dev/random | tr -d ' \n')"
-    if mkdir -m 700 "$name" 2>/dev/null; then
-      echo "$name"
-      break
-    fi
-  done
-}
-
 cd "$(git rev-parse --show-toplevel)"
-temp="$(mk_temp_dir)"
-trap "rm -rf $temp" EXIT
 
 NUM_RULES=89
-i=1
-while [ "$i" -le "$NUM_RULES" ]; do
-  echo "$i" >>"$temp/want"
-  i=$((i + 1))
-done
+seq 1 "$NUM_RULES" > want.tmp
 
-git grep -ho 'SML Definition ([[:digit:]]*)' crates/statics/src |
-  tr -d 'A-Za-z ()' |
-  sort -nu >"$temp/got"
+RE='SML Definition \(([[:digit:]]+)\)'
+git grep -hoE "$RE" crates/statics/src | sed -E "s#$RE#\1#" | sort -nu > got.tmp
 
-comm -23 "$temp/want" "$temp/got" >"$temp/want_not_got"
-if [ -s "$temp/want_not_got" ]; then
-  echo "the following rules were not referenced:"
-  cat "$temp/want_not_got"
-  exit 1
-fi
-
-comm -13 "$temp/want" "$temp/got" >"$temp/got_not_want"
-if [ -s "$temp/got_not_want" ]; then
-  echo "the following rules were unexpectedly referenced:"
-  cat "$temp/got_not_want"
-  exit 1
-fi
+diff want.tmp got.tmp
 
 echo "all and only rules 1-$NUM_RULES were referenced"
+rm want.tmp got.tmp
