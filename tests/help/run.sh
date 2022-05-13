4261ae0421c025a48158ac341a034c68228feb89
Ariel Davis <ariel.z.davis@icloud.com>
2022-05-12 17:00:29 -0700

    Rm run.sh tests

--- tests/help/run.sh
+++
@@ -1,3 +0,0 @@
-"$MILLET" --help >out.tmp
-diff ../../crates/cli/src/help.txt out.tmp
-rm out.tmp
--- tests/missing-file/run.sh
+++
@@ -1,3 +0,0 @@
-if "$MILLET" does-not-exist.sml >out.tmp; then exit 1; fi
-diff out.txt out.tmp
-rm out.tmp
