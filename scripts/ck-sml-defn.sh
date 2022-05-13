7c66f9b880b9b8d69e4455360ac9c732e4b4ab53
Ariel Davis <ariel.z.davis@icloud.com>
2022-05-12 18:18:11 -0700

    Make ck-sml-defn quiet

--- scripts/ck-sml-defn.sh
+++ scripts/ck-sml-defn.sh
@@ -3,13 +3,10 @@
 set -eu
 cd "$(git rev-parse --show-toplevel)"
 
-NUM_RULES=89
-seq 1 "$NUM_RULES" > want.tmp
+seq 1 89 > want.tmp
 
 RE='SML Definition \(([[:digit:]]+)\)'
 git grep -hoE "$RE" crates/statics/src | sed -E "s#$RE#\1#" | sort -nu > got.tmp
 
 diff want.tmp got.tmp
-
-echo "all and only rules 1-$NUM_RULES were referenced"
 rm want.tmp got.tmp
