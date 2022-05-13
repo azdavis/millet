ca9fcdbffe3beef0beaca41cb437dc6edf721865
Ariel Davis <ariel.z.davis@icloud.com>
2022-05-12 18:40:38 -0700

    Improve check

--- crates/tests/src/check.rs
+++ crates/tests/src/check.rs
@@ -2,11 +2,21 @@ use loc::Located;
 use rustc_hash::FxHashMap;
 use std::ops::Range;
 
-/// pass this the string of an sml program with some expectation comments. like this:
+/// pass the string of an SML program with some expectation comments.
 ///
-/// ```sml
-/// val x = foo bar
-/// (**         ^^^ message about bar *)
+/// expectation comments are regular SML comments except they:
+/// - are always on only one line
+/// - start with `(**`
+/// - point at the things that should have errors
+/// - contain the expected error message for those things
+///
+/// you might want to use raw string syntax (`r#"..."#`) to construct the string to pass.
+///
+/// ```ignore
+/// check(r#"
+/// val foo = bar quz
+/// (**           ^^^ message about quz *)
+/// "#);
 /// ```
 pub(crate) fn check(s: &str) {
   let indices: Vec<_> = s
@@ -17,7 +27,7 @@ pub(crate) fn check(s: &str) {
   let mut want: FxHashMap<_, _> = s
     .lines()
     .enumerate()
-    .filter_map(|(line_n, line_s)| get_expect(line_n, line_s))
+    .filter_map(|(line_n, line_s)| get_expect_comment(line_n, line_s))
     .collect();
   if let Err(err) = check_impl(s) {
     let range = Range::from(err.loc);
@@ -55,8 +65,8 @@ fn check_impl(s: &str) -> Result<(), Located<String>> {
   Ok(())
 }
 
-/// see [`get_expect`].
-const EXPECT_START: &str = "(**";
+/// see [`get_expect_comment`].
+const EXPECT_COMMENT_START: &str = "(**";
 
 #[derive(Debug, PartialEq, Eq, Hash)]
 struct Region {
@@ -67,10 +77,11 @@ struct Region {
 /// parses expectation comments from a line of text. the line will be the following in order:
 ///
 /// - zero or more of any character
-/// - the string EXPECT_START (the comment start)
+/// - the string EXPECT_COMMENT_START (the comment start)
 /// - zero or more spaces
-/// - one of `^` or `v` (the arrow)
-/// - zero or more non-spaces (the column range for the arrow)
+/// - one of `^` or `v` (the arrow character)
+/// - zero or more non-spaces (the column range for the arrow. usually these are all the same as the
+///   arrow character)
 /// - one or more spaces
 /// - one or more of any character (the message)
 /// - zero or more spaces
@@ -80,22 +91,22 @@ struct Region {
 /// if yes this returns Some((line, col_range, msg)), else returns None.
 ///
 /// note the arrows might be a little wonky with non-ascii.
-fn get_expect(line_n: usize, line_s: &str) -> Option<(Region, &str)> {
-  let (before, inner) = line_s.split_once(EXPECT_START)?;
+fn get_expect_comment(line_n: usize, line_s: &str) -> Option<(Region, &str)> {
+  let (before, inner) = line_s.split_once(EXPECT_COMMENT_START)?;
   let (inner, _) = inner.split_once("*)")?;
-  let non_space = inner.find(|c| c != ' ')?;
-  let inner = &inner[non_space..];
+  let non_space_idx = inner.find(|c| c != ' ')?;
+  let inner = &inner[non_space_idx..];
   let (col_range, msg) = inner.split_once(' ')?;
-  let line = match inner.chars().next()? {
+  let line = match col_range.chars().next()? {
     '^' => line_n - 1,
     'v' => line_n + 1,
     c => panic!("invalid arrow: {c}"),
   };
-  let start = before.len() + EXPECT_START.len() + non_space;
+  let start = before.len() + EXPECT_COMMENT_START.len() + non_space_idx;
   let end = start + col_range.len();
   let region = Region {
     line,
     col: start..end,
   };
-  Some((region, msg.trim()))
+  Some((region, msg.trim_end_matches(' ')))
 }
