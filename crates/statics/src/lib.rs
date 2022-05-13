d3c99b59259fe3d715d293e8de46f37e1db0978f
Ariel Davis <ariel.z.davis@icloud.com>
2022-05-13 02:12:16 -0700

    Add new statics

---
+++ crates/statics/src/lib.rs
@@ -0,0 +1,9 @@
+//! Static analysis. TODO.
+//!
+//! With help from [this article][1].
+//!
+//! [1]: http://dev.stephendiehl.com/fun/006_hindley_milner.html
+
+#![deny(missing_debug_implementations)]
+#![deny(missing_docs)]
+#![deny(rust_2018_idioms)]
