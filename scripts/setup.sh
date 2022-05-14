8f3bd3ab6008668d98babe1a1b38005d054a4def
Ariel Davis <ariel.z.davis@icloud.com>
2022-05-14 13:00:20 -0700

    Use vars with need_cmd

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,11 +10,14 @@ need_cmd() {
 need_cmd 'git' 'know where the top level dir is' 'https://git-scm.com'
 cd "$(git rev-parse --show-toplevel)"
 
-need_cmd 'rustc' 'compile rust' 'https://rustup.rs'
-need_cmd 'cargo' 'manage rust projects' 'https://rustup.rs'
-need_cmd 'rustfmt' 'format rust' 'https://rustup.rs'
-need_cmd 'node' 'run node scripts' 'https://nodejs.org/en/download'
-need_cmd 'npm' 'install node deps' 'https://nodejs.org/en/download'
+rustup='https://rustup.rs'
+need_cmd 'rustc' 'compile rust' "$rustup"
+need_cmd 'cargo' 'manage rust projects' "$rustup"
+need_cmd 'rustfmt' 'format rust' "$rustup"
+
+node='https://nodejs.org/en/download'
+need_cmd 'node' 'run node scripts' "$node"
+need_cmd 'npm' 'install node deps' "$node"
 
 cargo build
 cd extensions/vscode
