4c95dee6a708b4776ce87e03192d22818886c22c
Ariel Davis <ariel.z.davis@icloud.com>
2022-05-15 00:18:05 -0700

    Lower ParenTy

--- crates/lower/src/ty.rs
+++ crates/lower/src/ty.rs
@@ -6,13 +6,14 @@ pub(crate) fn get(cx: &mut Cx, ty: Option<ast::Ty>) -> hir::TyIdx {
   cx.arenas.ty.alloc(ty)
 }
 
-fn get_(_: &mut Cx, ty: ast::Ty) -> Option<hir::Ty> {
-  match ty {
+fn get_(cx: &mut Cx, ty: ast::Ty) -> Option<hir::Ty> {
+  let ret = match ty {
     ast::Ty::TyVarTy(_) => todo!(),
     ast::Ty::RecordTy(_) => todo!(),
     ast::Ty::ConTy(_) => todo!(),
     ast::Ty::TupleTy(_) => todo!(),
     ast::Ty::FnTy(_) => todo!(),
-    ast::Ty::ParenTy(_) => todo!(),
-  }
+    ast::Ty::ParenTy(ty) => get_(cx, ty.ty()?)?,
+  };
+  Some(ret)
 }
