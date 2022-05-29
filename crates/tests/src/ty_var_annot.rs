3b8dc812f0a86e989fd01a97a078ee52a36c2543
Ariel Davis <ariel.z.davis@icloud.com>
2022-05-29 04:38:27 -0700

    Turn on more tests

--- crates/tests/src/ty_var_annot.rs
+++ crates/tests/src/ty_var_annot.rs
@@ -1,52 +1,47 @@
 use crate::check::check;
 
 #[test]
-#[ignore = "todo for new"]
 fn t_01() {
   check(
     r#"
 fun 'a f (x: 'a) = let val y = x in y false; y end
-(**                                 ^^^^^^^ not a function type: '22 *)
+(**                                 ^^^^^^^ mismatched types: expected 'a, found bool -> _ *)
 "#,
   );
 }
 
 #[test]
-#[ignore = "todo for new"]
 fn t_02() {
   check(
     r#"
 fun bar (x: int): unit = ()
 fun 'a f (id: 'a -> 'a) x = bar (id x)
-(**                         ^^^^^^^^^^ mismatched types: expected int, found '25 *)
+(**                         ^^^^^^^^^^ mismatched types: expected int -> unit, found 'a -> _ *)
 "#,
   );
 }
 
 #[test]
-#[ignore = "todo for new"]
 fn t_03() {
   check(
     r#"
 fun 'a f (id: 'a -> 'a) x = id x + 1
-(**                         ^^^^^^^^ mismatched types: expected '22, found int *)
+(**                         ^^^^^^^^ mismatched types: expected _ * _ -> _ with word, real, or int, found 'a * int -> _ *)
 "#,
   );
 }
 
 #[test]
-#[ignore = "todo for new"]
 fn t_04() {
   check(
     r#"
     val 'a _: 'a = false
-(** ^^^^^^^^^^^^^^^^^^^^ mismatched types: expected '22, found bool *)
+(** ^^^^^^^^^^^^^^^^^^^^ mismatched types: expected 'a, found bool *)
 "#,
   );
 }
 
 #[test]
-#[ignore = "todo for new"]
 fn t_05() {
   check(
     r#"
@@ -54,31 +49,29 @@ type 'a heh = 'a list
 datatype 'a bad = Bad of 'a
 val _: int heh = [1]
     val _: unit = Bad
-(** ^^^^^^^^^^^^^^^^^ mismatched types: expected unit, found '27 -> '27 bad *)
+(** ^^^^^^^^^^^^^^^^^ mismatched types: expected unit, found _ -> _ bad *)
 "#,
   );
 }
 
 #[test]
-#[ignore = "todo for new"]
 fn t_06() {
   check(
     r#"
 fun ('t, 'u) apply (f: 't -> 'u) (x: 't): 'u = f x
 val _ = apply op+ (1, false)
-(**     ^^^^^^^^^^^^^^^^^^^^ mismatched types: expected int, found bool *)
+(**     ^^^^^^^^^^^^^^^^^^^^ mismatched types: expected _ * _ -> _, found int * bool -> _ *)
 "#,
   );
 }
 
 #[test]
-#[ignore = "todo for new"]
 fn t_07() {
   check(
     r#"
 fun ('a, 'b) f (xs: 'a list) (x: 'b) =
     x :: xs
-(** ^^^^^^^ mismatched types: expected '23, found '22 *)
+(** ^^^^^^^ mismatched types: expected _ * _ list -> _ list, found _ * _ -> _ *)
 "#,
   );
 }
