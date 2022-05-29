fb995628c085e90a02c8f98406905e666c642f0e
Ariel Davis <ariel.z.davis@icloud.com>
2022-05-28 22:00:36 -0700

    Restructure get_str_dec, impl StrDec::Local
    Basically directly from dec.

--- crates/statics/src/top_dec.rs
+++ crates/statics/src/top_dec.rs
@@ -4,7 +4,11 @@ use crate::types::{Cx, Env};
 
 pub(crate) fn get(st: &mut St, cx: &mut Cx, ars: &hir::Arenas, top_dec: &hir::TopDec) {
   match top_dec {
-    hir::TopDec::Str(str_dec) => get_str_dec(st, cx, ars, *str_dec),
+    hir::TopDec::Str(str_dec) => {
+      let mut env = Env::default();
+      get_str_dec(st, cx, ars, &mut env, *str_dec);
+      cx.env.extend(env);
+    }
     hir::TopDec::Sig(_) => {
       // TODO
     }
@@ -14,26 +18,36 @@ pub(crate) fn get(st: &mut St, cx: &mut Cx, ars: &hir::Arenas, top_dec: &hir::To
   }
 }
 
-pub(crate) fn get_str_dec(st: &mut St, cx: &mut Cx, ars: &hir::Arenas, str_dec: hir::StrDecIdx) {
+pub(crate) fn get_str_dec(
+  st: &mut St,
+  cx: &Cx,
+  ars: &hir::Arenas,
+  env: &mut Env,
+  str_dec: hir::StrDecIdx,
+) {
   let str_dec = match str_dec {
     Some(x) => x,
     None => return,
   };
   match &ars.str_dec[str_dec] {
-    hir::StrDec::Dec(dec) => {
-      let mut env = Env::default();
-      dec::get(st, cx, ars, &mut env, *dec);
-      cx.env.extend(env);
-    }
+    hir::StrDec::Dec(dec) => dec::get(st, cx, ars, env, *dec),
     hir::StrDec::Structure(_) => {
       // TODO
     }
-    hir::StrDec::Local(_, _) => {
-      // TODO
+    hir::StrDec::Local(local_dec, in_dec) => {
+      let mut local_env = Env::default();
+      get_str_dec(st, cx, ars, &mut local_env, *local_dec);
+      let mut cx = cx.clone();
+      cx.env.extend(local_env);
+      get_str_dec(st, &cx, ars, env, *in_dec);
     }
     hir::StrDec::Seq(str_decs) => {
+      let mut cx = cx.clone();
       for &str_dec in str_decs {
-        get_str_dec(st, cx, ars, str_dec);
+        let mut one_env = Env::default();
+        get_str_dec(st, &cx, ars, &mut one_env, str_dec);
+        cx.env.extend(one_env.clone());
+        env.extend(one_env);
       }
     }
   }
