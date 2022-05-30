42874bdc237803b82a0de4b56b5b4d2bbee606b6
Ariel Davis <ariel.z.davis@icloud.com>
2022-05-29 23:37:54 -0700

    Don't require URL in analysis

--- crates/analysis/src/lib.rs
+++ crates/analysis/src/lib.rs
@@ -8,32 +8,31 @@ use syntax::{ast::AstNode as _, rowan::TextRange};
 
 type SyntaxNodePtr = syntax::ast::SyntaxNodePtr<syntax::SML>;
 
-use url::Url;
-
 /// An error.
 #[derive(Debug)]
 pub struct Error {
-  /// The URL for the error.
-  pub url: Url,
   /// The range of the error.
   pub range: TextRange,
   /// The message of the error.
   pub message: String,
 }
 
-/// Turn a sequence of files into a sequence of errors.
+/// Returns a Vec of Vec of errors for each file.
+///
+/// The length of the returned Vec will be the same as the length of files.
 ///
 /// TODO should probably have this be more like a data structure that can support adding/removing
 /// files and other queries other than errors. Then we could be more incremental.
-pub fn get(files: Vec<(Url, String)>) -> Vec<Error> {
+pub fn get<'a, I>(files: I) -> Vec<Vec<Error>>
+where
+  I: Iterator<Item = &'a str>,
+{
   let mut files: Vec<_> = files
-    .into_iter()
-    .map(|(url, contents)| {
-      let lexed = lex::get(&contents);
+    .map(|contents| {
+      let lexed = lex::get(contents);
       let parsed = parse::get(&lexed.tokens);
       let lowered = lower::get(&parsed.root);
       AnalyzedFile {
-        url,
         lex_errors: lexed.errors,
         parsed,
         lowered,
@@ -46,38 +45,33 @@ pub fn get(files: Vec<(Url, String)>) -> Vec<Error> {
     statics::get(&mut st, &file.lowered.arenas, &file.lowered.top_decs);
     file.statics_errors = std::mem::take(&mut st.errors);
   }
-  // can't use flat map because of borrowing file.url
-  let mut ret = Vec::new();
-  for file in files.into_iter() {
-    ret.extend(
+  files
+    .into_iter()
+    .map(|file| {
       std::iter::empty()
         .chain(file.lex_errors.into_iter().map(|err| Error {
-          url: file.url.clone(),
           range: err.range,
           message: err.kind.to_string(),
         }))
         .chain(file.parsed.errors.into_iter().map(|err| Error {
-          url: file.url.clone(),
           range: err.range,
           message: err.kind.to_string(),
         }))
         .chain(file.lowered.errors.into_iter().map(|err| Error {
-          url: file.url.clone(),
           range: err.range,
           message: err.kind.to_string(),
         }))
         .chain(file.statics_errors.into_iter().filter_map(|err| {
           Some(Error {
-            url: file.url.clone(),
             range: get_statics_ptr(&file.lowered.ptrs, err.idx())?
               .to_node(file.parsed.root.syntax())
               .text_range(),
             message: err.display(&st.syms).to_string(),
           })
-        })),
-    );
-  }
-  ret
+        }))
+        .collect()
+    })
+    .collect()
 }
 
 fn get_statics_ptr(ptrs: &lower::Ptrs, idx: statics::Idx) -> Option<SyntaxNodePtr> {
@@ -92,7 +86,6 @@ fn get_statics_ptr(ptrs: &lower::Ptrs, idx: statics::Idx) -> Option<SyntaxNodePt
 }
 
 struct AnalyzedFile {
-  url: Url,
   lex_errors: Vec<lex::Error>,
   parsed: parse::Parse,
   lowered: lower::Lower,
