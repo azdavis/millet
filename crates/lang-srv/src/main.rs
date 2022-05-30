d9b32e744e4f4420c97a29424a0e5f51f222295b
Ariel Davis <ariel.z.davis@icloud.com>
2022-05-29 17:16:18 -0700

    Say millet

--- crates/lang-srv/src/main.rs
+++ crates/lang-srv/src/main.rs
@@ -4,7 +4,7 @@ use lsp_types::{HoverProviderCapability, ServerCapabilities};
 mod main_loop;
 
 fn main() -> anyhow::Result<()> {
-  eprintln!("startup lsp server");
+  eprintln!("startup millet lsp server");
   let (connection, io_threads) = Connection::stdio();
   let server_capabilities = ServerCapabilities {
     // not actually true
@@ -14,6 +14,6 @@ fn main() -> anyhow::Result<()> {
   let params = connection.initialize(serde_json::to_value(&server_capabilities)?)?;
   main_loop::run(connection, serde_json::from_value(params)?)?;
   io_threads.join()?;
-  eprintln!("shutdown lsp server");
+  eprintln!("shutdown millet lsp server");
   Ok(())
 }
