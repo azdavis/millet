6ae0860b34c50c519eed981eb990e3389b6a04ce
Ariel Davis <ariel.z.davis@icloud.com>
2022-06-06 00:05:24 -0700

    Doc

--- crates/cm/src/lib.rs
+++ crates/cm/src/lib.rs
@@ -1,5 +1,6 @@
 //! Handle a CM file.
 
+#![deny(missing_debug_implementations)]
 #![deny(rust_2018_idioms)]
 
 #[cfg(test)]
@@ -10,7 +11,10 @@ mod lower;
 mod parse;
 mod types;
 
-pub fn get(s: &str) -> anyhow::Result<(Vec<types::Export>, Vec<std::path::PathBuf>)> {
+pub use types::{Export, Name, Namespace};
+
+/// Turn the contents of a CM file into exports and members.
+pub fn get(s: &str) -> anyhow::Result<(Vec<Export>, Vec<std::path::PathBuf>)> {
   let tokens = lex::get(s)?;
   let root = parse::get(&tokens)?;
   let members = lower::get(root)?;
