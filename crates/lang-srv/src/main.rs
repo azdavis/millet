baa0953afe779c07120af3e15e16df9718509e83
Ariel Davis <ariel.z.davis@icloud.com>
2022-06-06 23:06:00 -0700

    Merge a file

--- crates/lang-srv/src/main.rs
+++ crates/lang-srv/src/main.rs
@@ -1,17 +1,39 @@
-use lsp_server::Connection;
-use lsp_types::ServerCapabilities;
-
-mod main_loop;
 mod state;
+
+fn run(conn: lsp_server::Connection, init: lsp_types::InitializeParams) -> anyhow::Result<()> {
+  log::info!("startup main loop: {init:#?}");
+  let root = match &init.root_uri {
+    Some(x) => x.clone(),
+    None => {
+      log::info!("no root url");
+      return Ok(());
+    }
+  };
+  let mut state = state::State::new(root, conn.sender.clone());
+  for msg in conn.receiver.iter() {
+    match msg {
+      lsp_server::Message::Request(req) => {
+        if conn.handle_shutdown(&req)? {
+          log::info!("shutdown main loop");
+          return Ok(());
+        }
+        state.handle_request(req);
+      }
+      lsp_server::Message::Response(res) => state.handle_response(res),
+      lsp_server::Message::Notification(notif) => state.handle_notification(notif),
+    }
+  }
+  Ok(())
+}
 
 fn main() -> anyhow::Result<()> {
   log::info!("startup millet lsp server");
-  let (connection, io_threads) = Connection::stdio();
-  let server_capabilities = ServerCapabilities {
+  let (connection, io_threads) = lsp_server::Connection::stdio();
+  let server_capabilities = lsp_types::ServerCapabilities {
     ..Default::default()
   };
   let params = connection.initialize(serde_json::to_value(&server_capabilities)?)?;
-  main_loop::run(connection, serde_json::from_value(params)?)?;
+  run(connection, serde_json::from_value(params)?)?;
   io_threads.join()?;
   log::info!("shutdown millet lsp server");
   Ok(())
