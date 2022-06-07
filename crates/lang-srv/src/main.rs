547d03b80c06f6be62b6338dfcad82a2e6dd9e92
Ariel Davis <ariel.z.davis@icloud.com>
2022-06-06 23:40:19 -0700

    Use a logger

--- crates/lang-srv/src/main.rs
+++ crates/lang-srv/src/main.rs
@@ -27,6 +27,7 @@ fn run(conn: lsp_server::Connection, init: lsp_types::InitializeParams) -> anyho
 }
 
 fn main() -> anyhow::Result<()> {
+  simple_logger::init_with_level(log::Level::Warn)?;
   log::info!("startup millet lsp server");
   let (connection, io_threads) = lsp_server::Connection::stdio();
   let server_capabilities = lsp_types::ServerCapabilities {
