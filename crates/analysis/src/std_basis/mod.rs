84b1b2811fac475b1486f1ddfc04f8529a29cb6f
Ariel Davis <ariel.z.davis@icloud.com>
2022-06-13 00:08:41 -0700

    Add some std_basis files

--- crates/analysis/src/std_basis/mod.rs
+++ crates/analysis/src/std_basis/mod.rs
@@ -1,18 +1,30 @@
-const ORDER: [&str; 0] = [];
+const ORDER: &[&str] = &[
+  include_str!("list-pair.sml"),
+  include_str!("list.sml"),
+  include_str!("option.sml"),
+];
 
 pub(crate) fn get_statics() -> statics::Statics {
   let mut ret = statics::Statics::default();
   let mode = statics::Mode::Declaration;
-  for contents in ORDER {
+  for &contents in ORDER {
     let lexed = lex::get(contents);
-    assert!(lexed.errors.is_empty());
+    if let Some(e) = lexed.errors.first() {
+      panic!("std_basis error: {}", e.kind);
+    }
     let parsed = parse::get(&lexed.tokens);
-    assert!(parsed.errors.is_empty());
+    if let Some(e) = parsed.errors.first() {
+      panic!("std_basis error: {}", e.kind);
+    }
     let mut lowered = lower::get(&parsed.root);
-    assert!(lowered.errors.is_empty());
+    if let Some(e) = lowered.errors.first() {
+      panic!("std_basis error: {}", e.kind);
+    }
     ty_var_scope::get(&mut lowered.arenas, &lowered.top_decs);
     statics::get(&mut ret, mode, &lowered.arenas, &lowered.top_decs);
-    assert!(ret.errors.is_empty());
+    if let Some(e) = ret.errors.first() {
+      panic!("std_basis error: {}", e.display(&ret.syms));
+    }
   }
   ret
 }
