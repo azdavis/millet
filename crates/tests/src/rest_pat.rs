b50494acbe710e34b95a32f4973b0cddaa76c265
Ariel Davis <ariel.z.davis@icloud.com>
2022-06-21 18:53:42 -0500

    Add more failing rest pat tests

--- crates/tests/src/rest_pat.rs
+++ crates/tests/src/rest_pat.rs
@@ -1,11 +1,40 @@
-use crate::check::check;
+use crate::check::fail;
 
 #[test]
-fn selector() {
-  check(
+fn multi_rest() {
+  fail(
     r#"
-val _: int = #1 (3, "hi")
-(**          ^^ unsupported language construct: `...` pattern rows *)
+val _ = fn {a, ..., ...} => 3
+(**        ^^^^^^^^^^^^^ cannot have multiple `...` *)
+"#,
+  );
+}
+
+#[test]
+fn rest_not_last() {
+  fail(
+    r#"
+val _ = fn {..., a} => 3
+(**        ^^^^^^^^ `...` must come last *)
+"#,
+  );
+}
+
+#[test]
+fn selector_smoke() {
+  fail(
+    r#"
+val _ : int = #1 (3, "hi")
+"#,
+  );
+}
+
+#[test]
+fn as_rest() {
+  fail(
+    r#"
+fun combine {a, b} = a + b
+val f = fn x as {b, ...} => combine x + 4 + b
 "#,
   );
 }
