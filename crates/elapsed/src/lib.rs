e17b6b7c9eff5c573c981517a3ee16b0c85884c9
Ariel Davis <ariel.z.davis@icloud.com>
2022-06-23 03:05:49 -0500

    Swap

--- crates/elapsed/src/lib.rs
+++ crates/elapsed/src/lib.rs
@@ -6,16 +6,6 @@
 
 use std::time::{Duration, Instant};
 
-/// Calls `f` and logs the message and the time it took to do so at the Info level.
-pub fn log<F, T>(msg: &str, f: F) -> T
-where
-  F: FnOnce() -> T,
-{
-  let (ret, elapsed) = time(f);
-  log::info!("{msg}: {elapsed:?}");
-  ret
-}
-
 /// Calls `f` and returns the result and the duration it took to do the call.
 pub fn time<F, T>(f: F) -> (T, Duration)
 where
@@ -25,4 +15,14 @@ where
   let res = f();
   let elapsed = Instant::now().duration_since(start);
   (res, elapsed)
+}
+
+/// Calls `f` and logs the message and the time it took to do so at the Info level.
+pub fn log<F, T>(msg: &str, f: F) -> T
+where
+  F: FnOnce() -> T,
+{
+  let (ret, elapsed) = time(f);
+  log::info!("{msg}: {elapsed:?}");
+  ret
 }
