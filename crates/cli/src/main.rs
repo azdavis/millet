5283f883ed0ebf037a3978f6facc9d5c4007cdce
Ariel Davis <ariel.z.davis@icloud.com>
2022-07-04 13:08:22 -0700

    Give help and require dir for cli

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,13 +1,34 @@
 use anyhow::{bail, Result};
 use paths::FileSystem as _;
+use pico_args::Arguments;
+
+fn usage() {
+  let current_exe_name = std::env::current_exe()
+    .ok()
+    .and_then(|x| x.file_name().map(|x| x.to_string_lossy().into_owned()))
+    .unwrap_or_else(|| "<unknown>".to_owned());
+  println!("usage:");
+  println!("  {current_exe_name} [options] <path>");
+  println!();
+  println!("options:");
+  println!("  -h, --help");
+  println!("    show this help");
+  println!();
+  println!("arguments:");
+  println!("  <path>");
+  println!("    directory path of the project to analyze. the directory should contain either a");
+  println!("    single .cm file or a millet.toml config file.");
+}
 
 fn main() -> Result<()> {
+  let mut args = Arguments::from_env();
+  if args.contains(["-h", "--help"]) {
+    usage();
+    return Ok(());
+  }
+  let root: String = args.free_from_str()?;
   let fs = paths::RealFileSystem::default();
-  let root = match std::env::args().nth(1) {
-    None => fs.canonicalize(std::env::current_dir()?.as_path())?,
-    Some(s) => fs.canonicalize(std::path::Path::new(&s))?,
-  };
-  let mut root = paths::Root::new(root);
+  let mut root = paths::Root::new(fs.canonicalize(std::path::Path::new(&root))?);
   let inp = analysis::get_input(&fs, &mut root)?;
   let mut an = analysis::Analysis::new(analysis::StdBasis::full());
   let got = an.get_many(&inp);
