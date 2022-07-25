7a6beac55dfbfc856aa9b9052c025e8b72c41426
Ariel Davis <ariel.z.davis@icloud.com>
2022-07-25 02:54:06 -0700

    Do not use anyhow for cli

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,4 +1,3 @@
-use anyhow::{bail, Result};
 use paths::FileSystem as _;
 use pico_args::Arguments;
 
@@ -23,25 +22,50 @@ fn usage() {
   println!("    - a .cm or .mlb file");
 }
 
-fn main() -> Result<()> {
+fn run() -> bool {
   let mut args = Arguments::from_env();
   if args.contains(["-h", "--help"]) {
     usage();
-    return Ok(());
+    return true;
   }
-  let path: String = args.free_from_str()?;
+  let path: String = match args.free_from_str() {
+    Ok(x) => x,
+    Err(e) => {
+      println!("error: {e}");
+      return false;
+    }
+  };
   let fs = paths::RealFileSystem::default();
-  let path = fs.canonicalize(std::path::Path::new(&path))?;
+  let path = match fs.canonicalize(std::path::Path::new(&path)) {
+    Ok(x) => x,
+    Err(e) => {
+      println!("{path}: error: {e}");
+      return false;
+    }
+  };
   let (root_path, root_group) =
     match analysis::input::GroupPath::new(&fs, path.clone().into_path_buf()) {
       None => (path, None),
       Some(path) => {
         let parent = path.as_path().parent().expect("no parent");
-        (fs.canonicalize(parent)?, Some(path))
+        let rp = fs
+          .canonicalize(parent)
+          .expect("canonicalize parent of canonical path");
+        (rp, Some(path))
       }
     };
   let mut root = paths::Root::new(root_path);
-  let inp = analysis::input::get(&fs, &mut root, root_group)?;
+  let inp = match analysis::input::get(&fs, &mut root, root_group) {
+    Ok(x) => x,
+    Err(e) => {
+      print!("{}", e.path().display());
+      if let Some(r) = e.range() {
+        print!(":{}", r.start);
+      }
+      print!(": error: {}", e.message());
+      return false;
+    }
+  };
   let mut an = analysis::Analysis::new(analysis::StdBasis::full(), config::ErrorLines::Many);
   let got = an.get_many(&inp);
   let num_errors: usize = got.iter().map(|(_, errors)| errors.len()).sum();
@@ -56,8 +80,15 @@ fn main() -> Result<()> {
   }
   if num_errors == 0 {
     println!("no errors!");
-    Ok(())
+    true
   } else {
-    bail!("{num_errors} errors");
+    println!("{num_errors} errors");
+    false
+  }
+}
+
+fn main() {
+  if !run() {
+    std::process::exit(1)
   }
 }
