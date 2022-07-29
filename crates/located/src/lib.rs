3952c80c4cb29d6085d2997900a10fa100bf456c
Ariel Davis <ariel.z.davis@icloud.com>
2022-07-29 04:13:11 -0700

    Deny more

--- crates/located/src/lib.rs
+++ crates/located/src/lib.rs
@@ -1,14 +1,20 @@
 //! A thin wrapper around [`text_size`] to add some helper functions and types.
 
+#![deny(missing_debug_implementations, missing_docs, rust_2018_idioms)]
+
 pub use text_size::{TextLen, TextRange, TextSize};
 
+/// A value located in a text file.
 #[derive(Debug, Clone, Copy)]
 pub struct Located<T> {
+  /// The value.
   pub val: T,
+  /// The location.
   pub range: text_size::TextRange,
 }
 
 impl<T> Located<T> {
+  /// Wrap a new value with the location from `self`.
   pub fn wrap<U>(&self, val: U) -> Located<U> {
     Located {
       val,
