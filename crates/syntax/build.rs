dc6ae16bb65971df50146907428862af17f78d11
Ariel Davis <ariel.z.davis@icloud.com>
2022-08-12 01:29:00 -0700

    Use a closure

--- crates/syntax/build.rs
+++ crates/syntax/build.rs
@@ -11,29 +11,6 @@ const SPECIAL: [(&str, &str); 7] = [
   ("StringLit", "a string literal"),
 ];
 
-fn get_token(s: &str) -> (TokenKind, Token) {
-  let kind: TokenKind;
-  let mut name: String;
-  let mut desc = None::<String>;
-  let doc = None::<String>;
-  if let Some(d) = SPECIAL.iter().find_map(|&(n, d)| (s == n).then_some(d)) {
-    kind = TokenKind::Special;
-    name = s.to_owned();
-    desc = Some(d.to_owned());
-  } else if s.chars().any(|c| c.is_ascii_alphabetic()) {
-    kind = TokenKind::Keyword;
-    name = snake_to_pascal(s);
-    name.push_str("Kw");
-  } else {
-    kind = TokenKind::Punctuation;
-    name = String::new();
-    for c in s.chars() {
-      name.push_str(char_name::get(c));
-    }
-  }
-  (kind, Token { name, desc, doc })
-}
-
 fn main() -> std::io::Result<()> {
   let out_dir = std::env::var("OUT_DIR").expect("OUT_DIR should be set");
   gen(
@@ -41,6 +18,27 @@ fn main() -> std::io::Result<()> {
     "SML",
     &["Whitespace", "BlockComment", "Invalid"],
     include_str!("syntax.ungram").parse().expect("ungram parse"),
-    get_token,
+    |s| {
+      let kind: TokenKind;
+      let mut name: String;
+      let mut desc = None::<String>;
+      let doc = None::<String>;
+      if let Some(d) = SPECIAL.iter().find_map(|&(n, d)| (s == n).then_some(d)) {
+        kind = TokenKind::Special;
+        name = s.to_owned();
+        desc = Some(d.to_owned());
+      } else if s.chars().any(|c| c.is_ascii_alphabetic()) {
+        kind = TokenKind::Keyword;
+        name = snake_to_pascal(s);
+        name.push_str("Kw");
+      } else {
+        kind = TokenKind::Punctuation;
+        name = String::new();
+        for c in s.chars() {
+          name.push_str(char_name::get(c));
+        }
+      }
+      (kind, Token { name, desc, doc })
+    },
   )
 }
