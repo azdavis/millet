d2f437ec6c0641c0b542ab6f52689b0116146fdf
Ariel Davis <ariel.z.davis@icloud.com>
2022-08-15 20:33:11 -0700

    Add another failing test

--- crates/tests/src/generalize.rs
+++ crates/tests/src/generalize.rs
@@ -1,7 +1,25 @@
 use crate::check::{check, fail};
 
 #[test]
-fn no_over_generalize_infer() {
+fn no_over_generalize_infer_val() {
+  fail(
+    r#"
+fun id x =
+  let val ret = x
+  in ret end
+
+val y = id ()
+val _ = y
+(**     ^ hover: unit *)
+val z = id 3
+val _ = z
+(**     ^ hover: int *)
+"#,
+  );
+}
+
+#[test]
+fn no_over_generalize_infer_fun() {
   fail(
     r#"
 fun id x =
