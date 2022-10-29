292ef39a4b622a03aa2d5ed22c1325cc7f186664
Ariel Davis <ariel.z.davis@icloud.com>
2022-10-28 20:14:59 -0700

    Allow no docs for config options

--- crates/config/src/lib.rs
+++ crates/config/src/lib.rs
@@ -80,15 +80,10 @@ pub enum ErrorLines {
 
 /// Optional settings for the server.
 #[derive(Debug, Deserialize)]
+#[allow(missing_docs)]
 pub struct Options {
-  /// Show information about tokens on hover.
   pub show_token_hover: bool,
-  /// Send diagnostics when file contents change before saving.
   pub diagnostics_on_change: bool,
-  /// **WARNING: THE FORMATTER IS HIGHLY EXPERIMENTAL. IT MAY IRREVOCABLY DESTROY SOME OR ALL OF
-  /// YOUR CODE.**
-  ///
-  /// Naively format open SML files on save.
   pub format: bool,
 }
 
