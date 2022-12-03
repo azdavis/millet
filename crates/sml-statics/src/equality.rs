a84d78ce6e700df8ffc55ca09d324d066bd6a08e
Ariel Davis <ariel.z.davis@icloud.com>
2022-12-03 12:59:25 -0800

    Add a debug check but do not enable bc it fails

--- crates/sml-statics/src/equality.rs
+++ crates/sml-statics/src/equality.rs
@@ -5,6 +5,9 @@
 /// TODO remove this and all false branches.
 pub const ENABLED: bool = true;
 
+/// TODO remove this and all false branches. currently it fails, possible because of `where type`?
+const ENABLE_DEBUG_CHECK: bool = false;
+
 use crate::types::{
   BasicOverload, Equality, Generalizable, Overload, RecordTy, SubstEntry, Sym, Ty, TyScheme,
   TyVarKind,
@@ -12,6 +15,7 @@ use crate::types::{
 use crate::{st::St, util::instantiate};
 use std::fmt;
 
+#[derive(Debug, PartialEq, Eq)]
 pub(crate) enum Ans {
   Yes,
   No(NotEqTy),
@@ -33,7 +37,7 @@ impl Ans {
 }
 
 /// A type that is not equality.
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 pub(crate) enum NotEqTy {
   FixedTyVar,
   Sym(Sym),
@@ -74,9 +78,9 @@ pub(crate) fn get_ty(st: &mut St, ty: &Ty) -> Ans {
         SubstEntry::Kind(kind) => match kind {
           TyVarKind::Equality => Ans::Yes,
           TyVarKind::Overloaded(ov) => match ov {
-            Overload::Basic(basic) => get_basic(basic),
+            Overload::Basic(basic) => get_basic(st, basic),
             Overload::Composite(comp) => {
-              Ans::all(comp.as_basics().iter().map(|&basic| get_basic(basic)))
+              Ans::all(comp.as_basics().iter().map(|&basic| get_basic(st, basic)))
             }
           },
           TyVarKind::Record(rows) => get_record(st, &rows),
@@ -91,11 +95,7 @@ pub(crate) fn get_ty(st: &mut St, ty: &Ty) -> Ans {
       }
     }
     Ty::Record(rows) => get_record(st, rows),
-    Ty::Con(args, sym) => match st.syms.equality(*sym) {
-      Equality::Always => Ans::Yes,
-      Equality::Sometimes => Ans::all(args.iter().map(|ty| get_ty(st, ty))),
-      Equality::Never => Ans::No(NotEqTy::Sym(*sym)),
-    },
+    Ty::Con(args, sym) => get_con(st, args, *sym),
     Ty::Fn(_, _) => Ans::No(NotEqTy::Fn),
   }
 }
@@ -104,19 +104,36 @@ fn get_record(st: &mut St, rows: &RecordTy) -> Ans {
   Ans::all(rows.values().map(|ty| get_ty(st, ty)))
 }
 
-/// NOTE: this is an optimization. The (ideally, if our assumptions are correct) equivalent but
-/// slower thing to do would be iterate over all the syms for the basic overload and check whether
-/// all of them are equality.
-///
-/// However, that should always return the same result as this because the signatures `INTEGER`,
-/// `WORD`, `STRING`, and `CHAR` all have their primary types (e.g. `int` for `INTEGER`) as
-/// `eqtype`s.
-fn get_basic(ov: BasicOverload) -> Ans {
+fn get_basic(st: &mut St, ov: BasicOverload) -> Ans {
+  let ret = get_basic_opt(ov);
+  // NOTE: this should always succeed because the signatures `INTEGER`, `WORD`, `STRING`, and `CHAR`
+  // all have their primary types (e.g. `int` for `INTEGER`) as `eqtype`s.
+  if ENABLE_DEBUG_CHECK {
+    assert_eq!(ret, get_basic_naive(st, ov));
+  }
+  ret
+}
+
+/// optimized but ideally logically equivalent form of [`get_basic_naive`].
+fn get_basic_opt(ov: BasicOverload) -> Ans {
   match ov {
     BasicOverload::Int | BasicOverload::Word | BasicOverload::String | BasicOverload::Char => {
       Ans::Yes
     }
     BasicOverload::Real => Ans::No(NotEqTy::Sym(Sym::REAL)),
+  }
+}
+
+fn get_basic_naive(st: &mut St, ov: BasicOverload) -> Ans {
+  let syms = st.syms.overloads()[ov].clone();
+  Ans::all(syms.into_iter().map(|sym| get_con(st, &[], sym)))
+}
+
+fn get_con(st: &mut St, args: &[Ty], sym: Sym) -> Ans {
+  match st.syms.equality(sym) {
+    Equality::Always => Ans::Yes,
+    Equality::Sometimes => Ans::all(args.iter().map(|ty| get_ty(st, ty))),
+    Equality::Never => Ans::No(NotEqTy::Sym(sym)),
   }
 }
 
