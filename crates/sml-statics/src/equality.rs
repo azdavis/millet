85b5fd1a0104eccd5a410fc89f4ea417c3812876
Ariel Davis <ariel.z.davis@icloud.com>
2022-12-03 13:54:53 -0800

    Skip equality checks for path order

--- crates/sml-statics/src/equality.rs
+++ crates/sml-statics/src/equality.rs
@@ -53,7 +53,7 @@ where
 ///
 /// Also sets any non-constrained meta type variables to be equality type variables.
 pub(crate) fn get_ty(st: &mut St, ty: &Ty) -> Result {
-  if !ENABLED {
+  if st.info.mode().is_path_order() || !ENABLED {
     return Ok(());
   }
   match ty {
@@ -129,11 +129,17 @@ fn get_con(st: &mut St, args: &[Ty], sym: Sym) -> Result {
 }
 
 pub(crate) fn get_ty_scheme(st: &mut St, ty_scheme: TyScheme) -> Result {
+  if st.info.mode().is_path_order() || !ENABLED {
+    return Ok(());
+  }
   let ty = instantiate(st, Generalizable::Always, ty_scheme);
   get_ty(st, &ty)
 }
 
 pub(crate) fn get_ty_info(st: &mut St, ty_info: TyInfo) -> Result {
+  if st.info.mode().is_path_order() || !ENABLED {
+    return Ok(());
+  }
   let is_ref = match &ty_info.ty_scheme.ty {
     Ty::Con(args, sym) => args.is_empty() && *sym == Sym::REF,
     _ => false,
