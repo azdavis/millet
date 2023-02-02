59db5c498f74ef2d83b0ee4067beba5dd2b7f5fb
Ariel Davis <ariel.z.davis@icloud.com>
2023-02-01 17:23:28 -0800

    Test no rebind it

--- crates/tests/src/cannot_rebind.rs
+++ crates/tests/src/cannot_rebind.rs
@@ -13,11 +13,21 @@ fun apply_eq (op =) a b = a = b
 }
 
 #[test]
-fn forbidden_binding() {
+fn ref_ctor() {
   check(
     r#"
-    datatype no = ref
-(** ^^^^^^^^^^^^^^^^^ cannot re-bind name: ref *)
+datatype no = ref
+(** + cannot re-bind name: ref *)
+"#,
+  );
+}
+
+#[test]
+fn val_it() {
+  check(
+    r#"
+val it = 3
+(** + cannot re-bind name: it *)
 "#,
   );
 }
