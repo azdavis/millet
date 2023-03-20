8b9bb5adb011510cfdaf4fb60a4196050d91f2c1
Ariel Davis <ariel.z.davis@icloud.com>
2023-03-20 03:24:01 -0700

    Add a dec test

--- crates/tests/src/disallowed.rs
+++ crates/tests/src/disallowed.rs
@@ -8,14 +8,26 @@ fn exp() {
 version = 1
 language.exp.while = false
 "#;
-  let mlb = r#"
-a.sml
-"#;
   let sml = r#"
 val _ = 4
 val _ = while true do ()
 (**     ^^^^^^^^^^^^^^^^ disallowed expression: while *)
 val _ = "hi"
 "#;
-  check_multi([(config::file::NAME, config), ("s.mlb", mlb), ("a.sml", sml)]);
+  check_multi([(config::file::NAME, config), ("s.mlb", "a.sml"), ("a.sml", sml)]);
+}
+
+#[test]
+fn dec() {
+  let config = r#"
+version = 1
+language.dec.signature = false
+"#;
+  let sml = r#"
+structure Str = struct end
+signature SIG = sig end
+(** + disallowed declaration: signature *)
+functor F() = struct end
+"#;
+  check_multi([(config::file::NAME, config), ("s.mlb", "a.sml"), ("a.sml", sml)]);
 }
