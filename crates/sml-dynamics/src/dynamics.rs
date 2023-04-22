c3ba967406cc0b7c5f266294179a1d1c51dc2e47
Ariel Davis <ariel.z.davis@icloud.com>
2023-04-22 14:45:50 -0700

    Allow many

--- crates/sml-dynamics/src/dynamics.rs
+++ crates/sml-dynamics/src/dynamics.rs
@@ -1,7 +1,7 @@
 //! The overall dynamics type.
 
 use crate::step::step;
-use crate::types::{Cx, St, Step};
+use crate::types::{Cx, FrameKind, St, Step};
 
 /// A runner of the dynamics.
 #[derive(Debug)]
@@ -12,10 +12,14 @@ pub struct Dynamics<'a> {
 }
 
 impl<'a> Dynamics<'a> {
-  /// Returns a new dynamics for this dec.
+  /// Returns a new dynamics for these decs, or `None` if `decs` is empty.
   #[must_use]
-  pub fn new(cx: Cx<'a>, dec: sml_hir::DecIdx) -> Self {
-    Self { cx, st: St::default(), step: Some(Step::Dec(dec)) }
+  pub fn new(cx: Cx<'a>, decs: Vec<sml_hir::DecIdx>) -> Option<Self> {
+    let mut st = St::default();
+    let mut decs = decs.into_iter();
+    let dec = decs.next()?;
+    st.push_with_cur_env(FrameKind::Local(decs, Vec::new().into_iter()));
+    Some(Self { cx, st, step: Some(Step::Dec(dec)) })
   }
 
   /// Takes a step, or no-ops if it's done.
