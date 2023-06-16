e6840e5c5211076276d16428290974c7f761fdad
Ariel Davis <ariel.z.davis@icloud.com>
2023-06-15 23:40:11 -0700

    Turn on completions

--- crates/lang-srv/src/capabilities.rs
+++ crates/lang-srv/src/capabilities.rs
@@ -1,9 +1,6 @@
 //! See [`get`].
 
 /// TODO set to true, then remove
-const COMPLETIONS: bool = false;
-
-/// TODO set to true, then remove
 const INLAY_HINTS: bool = false;
 
 /// Returns the capabilities of the server.
@@ -27,7 +24,7 @@ pub(crate) fn get() -> lsp_types::ServerCapabilities {
     document_formatting_provider: Some(lsp_types::OneOf::Left(true)),
     document_symbol_provider: Some(lsp_types::OneOf::Left(true)),
     references_provider: Some(lsp_types::OneOf::Left(true)),
-    completion_provider: COMPLETIONS.then(|| lsp_types::CompletionOptions {
+    completion_provider: Some(lsp_types::CompletionOptions {
       trigger_characters: Some(vec![".".to_owned()]),
       ..lsp_types::CompletionOptions::default()
     }),
