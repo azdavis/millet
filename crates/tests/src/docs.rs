5ec783636c69ec0d3982badf3a9302085485aa1e
Ariel Davis <ariel.z.davis@icloud.com>
2023-07-25 17:38:04 -0700

    Use @limit first

--- crates/tests/src/docs.rs
+++ crates/tests/src/docs.rs
@@ -6,20 +6,24 @@ use pulldown_cmark::{CodeBlockKind, Event, Options, Parser, Tag};
 
 const SML: &str = "sml";
 
+fn opts(limit: raw::Limit) -> raw::Opts<'static> {
+  raw::Opts {
+    std_basis: raw::StdBasis::Full,
+    outcome: raw::Outcome::Pass,
+    limit,
+    min_severity: Severity::Warning,
+    expected_input: raw::ExpectedInput::Good,
+  }
+}
+
 fn check_all(contents: &str) {
   let mut options = Options::empty();
   options.insert(Options::ENABLE_TABLES);
   let parser = Parser::new_ext(contents, options);
   let mut inside = false;
   let mut ignore_next = false;
+  let mut limit = raw::Limit::None;
   let mut ac = String::new();
-  let opts = raw::Opts {
-    std_basis: raw::StdBasis::Full,
-    outcome: raw::Outcome::Pass,
-    limit: raw::Limit::First,
-    min_severity: Severity::Warning,
-    expected_input: raw::ExpectedInput::Good,
-  };
   for ev in parser {
     match ev {
       Event::Start(Tag::CodeBlock(CodeBlockKind::Fenced(lang))) => {
@@ -30,11 +34,12 @@ fn check_all(contents: &str) {
       Event::End(Tag::CodeBlock(CodeBlockKind::Fenced(lang))) => {
         if lang.as_ref() == SML {
           if !ignore_next {
-            raw::get(raw::one_file_fs(ac.as_ref()), opts);
+            raw::get(raw::one_file_fs(ac.as_ref()), opts(limit));
           }
           ac.clear();
           inside = false;
           ignore_next = false;
+          limit = raw::Limit::None;
         }
       }
       Event::Text(s) => {
@@ -43,8 +48,11 @@ fn check_all(contents: &str) {
         }
       }
       Event::Html(s) => {
-        if s.trim().strip_prefix("<!-- @ignore ").and_then(|s| s.strip_suffix(" -->")).is_some() {
+        let s = s.trim();
+        if s.strip_prefix("<!-- @ignore ").and_then(|s| s.strip_suffix(" -->")).is_some() {
           ignore_next = true;
+        } else if s == "<!-- @limit first -->" {
+          limit = raw::Limit::First;
         }
       }
       _ => {}
