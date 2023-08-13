703d4a73a4ec921855c9a081f3336e8204023456
Ariel Davis <ariel.z.davis@icloud.com>
2023-08-12 20:38:27 -0700

    Add allow vector exp test

--- crates/tests/src/deviations/successor_ml/vector_exp_and_pat.rs
+++ crates/tests/src/deviations/successor_ml/vector_exp_and_pat.rs
@@ -1,6 +1,6 @@
 //! Vector expressions and patterns like `#[a, b]`.
 
-use crate::check::check;
+use crate::check::{check, check_multi, raw};
 
 #[test]
 fn exp_default_disallow() {
@@ -20,4 +20,18 @@ val _ = fn #[x, 2] => x | _ => 2
 (**        ^^^^^^^ vector patterns *)
 "#,
   );
+}
+
+#[test]
+fn exp_config_allow() {
+  let config = r#"
+version = 1
+language.successor-ml.vector = true
+"#;
+  let sml = r#"
+val x = #[1, 2]
+val () = x
+(** + expected `unit`, found `int vector` *)
+"#;
+  check_multi(raw::singleton(config, sml));
 }
