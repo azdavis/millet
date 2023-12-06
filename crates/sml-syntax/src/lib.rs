1d49c2f303bb287aa5c9865c6565fa2d7f85fa7a
Ariel Davis <ariel.z.davis@icloud.com>
2023-12-05 22:36:53 -0800

    Allow more

--- crates/sml-syntax/src/lib.rs
+++ crates/sml-syntax/src/lib.rs
@@ -1,11 +1,11 @@
 //! Types for working with SML syntax trees.
 
-#[allow(missing_debug_implementations, missing_docs)]
+#[allow(clippy::pedantic, missing_debug_implementations, missing_docs)]
 pub mod ast {
   include!(concat!(env!("OUT_DIR"), "/ast.rs"));
 }
 
-#[allow(missing_debug_implementations, missing_docs)]
+#[allow(clippy::pedantic, missing_debug_implementations, missing_docs)]
 pub mod kind {
   include!(concat!(env!("OUT_DIR"), "/kind.rs"));
 }
