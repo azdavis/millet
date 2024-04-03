5e0d53a80322100950f41d559b22e5902aac02db
Ariel Davis <ariel.z.davis@icloud.com>
2024-04-03 03:40:48 -0700

    Test some implicit ty var handling

--- crates/tests/src/deviations.rs
+++ crates/tests/src/deviations.rs
@@ -1,5 +1,6 @@
 //! Known deviations (or not) from the Definition.
 
+mod implicit_ty_var;
 mod mlton;
 mod smlnj;
 mod successor_ml;
---
+++ crates/tests/src/deviations/implicit_ty_var.rs
@@ -0,0 +1,59 @@
+//! Tests involving type variables which may be bound implicitly, where the behavior differs between
+//! some SML implementations.
+//!
+//! Tested with:
+//!
+//! - smlnj: 110.99.3
+//! - mlton: 20210117
+
+use crate::check::check;
+
+/// - smlnj: error
+/// - mlton: no error
+#[test]
+fn datatype() {
+  check(
+    r"
+fun f x =
+  let
+    datatype t = Poly of 'a
+(**                      ^^ undefined *)
+  in
+    Poly x; 4
+  end
+",
+  );
+}
+
+/// - smlnj: error
+/// - mlton: no error
+#[test]
+fn typ() {
+  check(
+    r"
+fun f x =
+  let
+    type t = 'a
+(**          ^^ undefined *)
+  in
+    x: t
+  end
+",
+  );
+}
+
+/// smlnj and mlton: no error
+#[test]
+fn exception() {
+  check(
+    r"
+fun f x =
+  let
+    exception Poly of 'a
+(**                   ^^ undefined *)
+  in
+    raise Poly x
+  end
+",
+  );
+}
